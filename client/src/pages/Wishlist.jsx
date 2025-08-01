3afbec3e34d11f2937488a7cf71312f24d77b22c
saif520 <saifuddin2k17@gmail.com>
2025-08-01 13:51:07 +0530

    latest update

--- client/src/pages/Wishlist.jsx
+++ client/src/pages/Wishlist.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/wishlist.css";
+import "../styles/Wishlist.css";
 import { getMyWishlist, removeFromWishlist, moveToCart } from "../services/wishlistServices";
 import MoveToCartModal from "../components/MoveToCartModal";
 import { toast } from "react-toastify"; // ✅ added toast import
