fd4bacc5b872b27ed39c50b09e0748e7a4ee610b
saif520 <saifuddin2k17@gmail.com>
2025-09-12 10:12:43 +0530

    fashion-zero updated

--- client/src/pages/Wishlist.jsx
+++ client/src/pages/Wishlist.jsx
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/wishlist.css";
+import "../styles/Wishlist.css";
 import {
   getMyWishlist,
   removeFromWishlist,
