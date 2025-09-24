f7adbb73d92c176cae1075fec6bf15547f5d478c
saif520 <saifuddin2k17@gmail.com>
2025-09-24 20:46:04 +0530

    send mail updated

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -5,6 +5,7 @@ export const sendEmail = async ({ email, subject, message }) => {
     host: process.env.SMTP_HOST,
     // service: process.env.SMTP_SERVICE,
     port: process.env.SMTP_PORT,
+    secure: false,
     auth: {
       user: process.env.SMTP_MAIL,
       pass: process.env.SMTP_PASSWORD,
