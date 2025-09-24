90cf94a0139cd68fe2021807f83987a595f5dcb3
saif520 <saifuddin2k17@gmail.com>
2025-09-24 16:39:57 +0530

    send mail updated

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -21,30 +21,26 @@
 // }; 
 
 
-import nodeMailer from "nodemailer";
+import sgMail from "@sendgrid/mail";
+
+import dotenv from "dotenv";
+dotenv.config();
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+console.log("SendGrid API key loaded:", process.env.SENDGRID_API_KEY ? "YES" : "NO");
+
 
 export const sendEmail = async ({ email, subject, message }) => {
   try {
-    const transporter = nodeMailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      secure: true, // required for port 465
-      auth: {
-        user: process.env.SMTP_MAIL,
-        pass: process.env.SMTP_PASSWORD,
-      },
-    });
-
-    const options = {
-      from: process.env.SMTP_MAIL,
+    const msg = {
       to: email,
+      from: process.env.SENDGRID_FROM, // must be verified in SendGrid
       subject,
       html: message,
     };
 
-    await transporter.sendMail(options);
+    await sgMail.send(msg);
     console.log("✅ Email sent to:", email);
   } catch (err) {
-    console.error("❌ Email send failed:", err.message);
+    console.error("❌ Email send failed:", err.response?.body || err.message);
   }
 };
